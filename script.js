219a3c6994f6235d16be85f58f16c4e64ca9d3ba
Kamilabiernaski <kamila.biernaski@escola.pr.gov.br>
2026-07-10 08:56:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Função do Quiz Agrinho 2026
 
+function verificarResposta(opcao) {
+
+    const resultado = document.getElementById("resultado");
+
+    if (opcao === "b") {
+        resultado.innerHTML =
+        "✅ Parabéns! A irrigação por gotejamento é uma das formas mais eficientes de economizar água na agricultura.";
+        resultado.style.color = "green";
+    } else {
+        resultado.innerHTML =
+        "❌ Resposta incorreta. A melhor alternativa é utilizar a irrigação por gotejamento, pois reduz o desperdício e utiliza a água de forma mais eficiente.";
+        resultado.style.color = "red";
+    }
+
+}
